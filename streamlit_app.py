d9c7bc4c9b714190fc23b1b68e9d59544418deae
7014gree <7014gree@gmail.com>
2024-07-01 21:17:28 +0100

    day8

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,50 @@
 import streamlit as st
-import numpy as np
-import altair as alt
-import pandas as pd
+from datetime import time, datetime
 
-st.header('st.write')
+# st.header('st.slider')
 
-st.write('Hello, *World*! :sunglasses:')
+# date = st.slider('Select Date', datetime(1900, 1, 1), datetime(2024, 12, 31))
 
-st.write(1234)
+# st.write("Date: ", date)
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-})
-st.write(df)
 
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+# amount = st.slider('Select Amount', 1, 100)
 
-df2 = pd.DataFrame(
-    np.random.randn(200, 3),
-    columns=['a', 'b', 'c']
+# st.write("Amount: ", amount)
+
+st.header('st.slider')
+
+
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 130, 25)
+
+st.write("I'm ", age, 'years old')
+
+
+st.subheader('Range slider')
+
+values = st.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
 )
-c = alt.Chart(df2).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
+st.write('Values:', values)
+
+
+st.subheader('Range time slider')
+
+appointment = st.slider(
+    "Schedule your appointment:",
+    value=(time(11, 30), time(12, 45))
 )
-st.write(c)
+st.write("You're scheduled for:", appointment)
+
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+    'When do you start?',
+    value=datetime(2020, 1, 1, 9, 30),
+    format="MM/DD/YY - hh:mm"
+)
+st.write("Start time:", start_time)
