c284063acbb3e534261c7d2331fb7345e50151d1
7014gree <7014gree@gmail.com>
2024-07-01 21:30:46 +0100

    day9

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,15 @@
 import streamlit as st
-from datetime import time, datetime
-
-# st.header('st.slider')
-
-# date = st.slider('Select Date', datetime(1900, 1, 1), datetime(2024, 12, 31))
-
-# st.write("Date: ", date)
+# from datetime import time, datetime
+import numpy as np
+# import altair as alt
+import pandas as pd
 
 
-# amount = st.slider('Select Amount', 1, 100)
+st.header('Line chart')
 
-# st.write("Amount: ", amount)
-
-st.header('st.slider')
-
-
-st.subheader('Slider')
-
-age = st.slider('How old are you?', 0, 130, 25)
-
-st.write("I'm ", age, 'years old')
-
-
-st.subheader('Range slider')
-
-values = st.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c']
 )
-st.write('Values:', values)
 
-
-st.subheader('Range time slider')
-
-appointment = st.slider(
-    "Schedule your appointment:",
-    value=(time(11, 30), time(12, 45))
-)
-st.write("You're scheduled for:", appointment)
-
-
-st.subheader('Datetime slider')
-
-start_time = st.slider(
-    'When do you start?',
-    value=datetime(2020, 1, 1, 9, 30),
-    format="MM/DD/YY - hh:mm"
-)
-st.write("Start time:", start_time)
+st.line_chart(chart_data)
